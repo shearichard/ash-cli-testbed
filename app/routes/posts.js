eaed37e1c22cc00a2d3050a7e5bb23b10b2ec7ee
Richard Shea <rshea@thecubagroup.com>
2017-03-26 23:53:57 +1300

    After changing the import back to the way it was created during [?25l[?25h[?25h[?25h[?25h

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,21 +1,7 @@
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//import Ash from '@ash-framework/ash'
-//
-//export default class PostRoute extends Ash.Route {
-//  model () {
-//
-//  }
-//}
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+import Ash from '@ash-framework/ash'
 
-// app/routes/posts.js
-import Ash from 'ash-core'
-
-export default class PostsRoute extends Ash.Route {
+export default class PostRoute extends Ash.Route {
   model () {
-    return [
-      {id: 1, title: 'My post 1', description: 'WoW'},
-      {id: 2, title: 'My post 2', description: 'WoWsErS'}
-    ]
+
   }
 }
