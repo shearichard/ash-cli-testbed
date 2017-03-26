e64db34e12014f846750a30599f54f47ed94d2f8
Richard Shea <rshea@thecubagroup.com>
2017-03-26 23:52:09 +1300

    After changes as per README

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -1,7 +1,21 @@
-import Ash from '@ash-framework/ash'
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//import Ash from '@ash-framework/ash'
+//
+//export default class PostRoute extends Ash.Route {
+//  model () {
+//
+//  }
+//}
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-export default class PostRoute extends Ash.Route {
+// app/routes/posts.js
+import Ash from 'ash-core'
+
+export default class PostsRoute extends Ash.Route {
   model () {
-
+    return [
+      {id: 1, title: 'My post 1', description: 'WoW'},
+      {id: 2, title: 'My post 2', description: 'WoWsErS'}
+    ]
   }
 }
